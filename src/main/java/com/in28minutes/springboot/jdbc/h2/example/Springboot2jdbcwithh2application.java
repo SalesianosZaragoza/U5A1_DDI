ccc20cee4f060aa92ff94985ab1ca84af19f0331
Gorka Sanz <gorka.sanz@salesianos.edu>
2023-11-13 09:36:17 +0100

    added files

---
+++ src/main/java/com/in28minutes/springboot/jdbc/h2/example/Springboot2jdbcwithh2application.java
@@ -0,0 +1,40 @@
+package com.in28minutes.springboot.jdbc.h2.example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.in28minutes.springboot.jdbc.h2.example.repopsitory.StudentRepopsitory;
+import com.in28minutes.springboot.jdbc.h2.example.student.Student;
+
+@SpringBootApplication
+public class Springboot2jdbcwithh2application implements CommandLineRunner {
+
+	private Logger logger = LoggerFactory.getlogger(this.getClass());
+
+	@Autowired
+	StudentRepopsitory repository;
+
+	public static void main(String[] args) {
+		springapplication.run(Springboot2jdbcwithh2application.class, args);
+	}
+
+	@Override
+    public void run(String...args) throws Exception {
+
+		logger.info("student id 10001 -> {}", repository.findbyid(10001l));
+
+        logger.info("all users 1 -> {}", repository.findall());
+
+		logger.info("inserting -> {}", repository.insert(new Student(10010l, "john", "a1234657")));
+
+		logger.info("update 10001 -> {}", repository.update(new Student(10001l, "name-updated", "new-passport")));
+
+        repository.deletebyid(10002l);
+
+        logger.info("all users 2 -> {}", repository.findall());
+
+    }
+}
---
+++ src/main/java/com/in28minutes/springboot/jdbc/h2/example/repopsitory/StudentRepopsitory.java
@@ -0,0 +1,51 @@
+package com.in28minutes.springboot.jdbc.h2.example.repopsitory;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import com.in28minutes.springboot.jdbc.h2.example.student.Student;
+
+class Studentrowmapper implements RowMapper<Student> {
+	public Student mapRow(ResultSet rs, int rowNum) throws SQLException {
+		Student student = new Student();
+		student.setid(rs.getLong("id"));
+		student.setname(rs.getString("name"));
+		student.setpassportnumber(rs.getString("passport_number"));
+		return student;
+	}
+
+}
+
+public class StudentRepopsitory {
+	@Autowired
+	JdbcTemplate jdbctemplate;
+
+	public Student findbyid(long id) {
+		return jdbctemplate.queryForObject("select * from student where id=?", new Object[] { id },
+				new BeanPropertyRowMapper<Student>(Student.class));
+	}
+
+	public List<Student> findall() {
+		return jdbctemplate.query("select * from student", new Studentrowmapper());
+	}
+
+	public int deletebyid(long id) {
+		return jdbctemplate.update("delete from student where id=?", new Object[] { id });
+	}
+
+	public int insert(Student student) {
+		return jdbctemplate.update("insert into student (id, name, passport_number) " + "values(?,  ?, ?)",
+				new Object[] { student.getid(), student.getname(), student.getpassportnumber() });
+	}
+
+	public int update(Student student) {
+		return jdbctemplate.update("update student " + " set name = ?, passport_number = ? " + " where id = ?",
+				new Object[] { student.getname(), student.getpassportnumber(), student.getid() });
+	}
+}
